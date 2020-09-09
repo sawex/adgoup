6232d950800ed4f692121c712ee6ef6c11c9e6e3
Danil <danil27zp@gmail.com>
2020-09-09 13:21:53 +0300

    (feature) create first page;

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 import './polyfill';
 import 'focus-visible/dist/focus-visible.min';
 import store from 'store';
+import Rellax from 'rellax';
 import smoothAnchors from './smooth-anchors';
 
 import { qs, qsAll } from './dom-helpers';
@@ -14,12 +15,24 @@ class Main {
     smoothAnchors();
 
     this._registerGlobalObjects();
+    this._initParalaxCard();
   }
 
   _registerGlobalObjects() {
     window.qs = qs;
     window.qsAll = qsAll;
     window.store = store;
+  }
+
+  _initParalaxCard() {
+    new Rellax('.rellax', {
+      speed: -2,
+      center: false,
+      wrapper: null,
+      round: true,
+      vertical: false,
+      horizontal: true,
+    });
   }
 }
 
