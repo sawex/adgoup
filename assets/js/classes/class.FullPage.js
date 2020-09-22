a032cdfcd895d80b18e4c5ae47a6f780ea47067a
Danil <danil27zp@gmail.com>
2020-09-22 19:51:33 +0300

    (feature) change cards effect;

--- assets/js/classes/class.FullPage.js
+++ assets/js/classes/class.FullPage.js
@@ -5,7 +5,7 @@ import { qs } from '../dom-helpers';
 
 class FullPage {
   constructor() {
-    this.TRANSLATE_GAP = 0;
+    // this.TRANSLATE_GAP = 0;
     this.isFullPageDisabled = false;
 
     this.cardList = qs('.why-us__cards-list');
@@ -15,7 +15,7 @@ class FullPage {
 
   _run() {
     this._initFullPage();
-    // this._initScrollingEventListener();
+    this._initScrollingEventListener();
   }
 
   _initFullPage() {
@@ -25,7 +25,7 @@ class FullPage {
       loopHorizontal: false,
       controlArrows: false,
       anchors: ['section1', 'section2', 'section3', 'section4'],
-      normalScrollElements: '.why-us__cards-list',
+      // normalScrollElements: '.why-us__cards-list',
       licenseKey: 'DB55ED36-24984ABE-A41951BD-7E95785D',
       scrollHorizontally: true,
       scrollHorizontallyKey: '0F51D536-0F9C4FFC-9EF22BD4-92F4E21',
@@ -45,23 +45,23 @@ class FullPage {
     window.addEventListener('resize', () => this._handleFullPageOnResize());
   }
 
-  _initCardsScrolling(e) {
-    if (window.matchMedia('(max-width: 1279px)').matches) return;
-
-    const DELTA = e.deltaY || e.detail || e.wheelDelta;
-
-    if (DELTA > 0) {
-      this.TRANSLATE_GAP -= 30;
-    } else {
-      this.TRANSLATE_GAP += 30;
-    }
-
-    this.cardList.style.transform = `translateX(${this.TRANSLATE_GAP}px)`;
-    this.cardList.style.WebkitTransform = `translateX(${this.TRANSLATE_GAP}px)`;
-    this.cardList.style.MsTransform = `translateX(${this.TRANSLATE_GAP}px)`;
-
-    e.preventDefault();
-  }
+  // _initCardsScrolling(e) {
+  //   if (window.matchMedia('(max-width: 1279px)').matches) return;
+  //
+  //   const DELTA = e.deltaY || e.detail || e.wheelDelta;
+  //
+  //   if (DELTA > 0) {
+  //     this.TRANSLATE_GAP -= 30;
+  //   } else {
+  //     this.TRANSLATE_GAP += 30;
+  //   }
+  //
+  //   this.cardList.style.transform = `translateX(${this.TRANSLATE_GAP}px)`;
+  //   this.cardList.style.WebkitTransform = `translateX(${this.TRANSLATE_GAP}px)`;
+  //   this.cardList.style.MsTransform = `translateX(${this.TRANSLATE_GAP}px)`;
+  //
+  //   e.preventDefault();
+  // }
 
   _handleFullPageOnResize() {
     if (window.matchMedia('(max-width: 1279px)').matches) {
